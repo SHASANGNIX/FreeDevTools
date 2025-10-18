194c1f3f7dbc0489687767fb0f7ac97cb8f7b72b
SHASANGNIX <shasanshah06@gmail.com>
2025-10-18 17:19:54 +0530

    Rename emoji files to be Windows-compatible

--- frontend/scripts/convert-to-webp.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -z "$1" ] || [ -z "$2" ]; then
-  echo "❌ Error: src and out are required"
-  echo "Usage: ./scripts/convert-to-webp.sh path/to/image.jpg path/to/image.webp"
-  exit 1
-fi
-
-cwebp -q 100 "$1" -o "$2"
--- search-index/go.mod
+++
@@ -1,5 +0,0 @@
-module search-index
-
-go 1.21
-
-require gopkg.in/yaml.v2 v2.4.0
